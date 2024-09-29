dfe32ed3283041ac1b6157dcbcc51c69db28a862
DarkMG1 <64174035+DarkMG1@users.noreply.github.com>
2024-09-29 00:49:11 -0400

    Fixed Mastodon Integration

---
+++ mastodon_parser.py
@@ -0,0 +1,33 @@
+from mastodon import Mastodon
+from bs4 import BeautifulSoup
+
+from helper import read_tokens
+
+
+def get_plain_text(html_content):
+  soup = BeautifulSoup(html_content, 'html.parser')
+  return soup.get_text()
+
+
+def get_toots_after_main_toot(mastodon_client, main_toot_id, limit=40):
+  """Fetches toots that are replies to the main toot on Mastodon.
+
+  Args:
+      mastodon_client: A Mastodon client instance.
+      main_toot_id: The ID of the main toot.
+      limit: The maximum number of toots to fetch.
+
+  Returns:
+      A list of toots that are replies to the main toot.
+  """
+
+  # Fetch the context of the main toot
+  response = mastodon_client.status_context(main_toot_id)
+
+  # Get the descendants (replies) of the main toot
+  toots = response["descendants"][:limit]
+
+  for toot in toots:
+    toot["content"] = get_plain_text(toot["content"])
+
+  return toots
